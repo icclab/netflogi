326ea4db36bc3519d53ad44e4c85b8257ac4fbe5
Luca Del Vecchio <delv@zhaw.ch>
2016-02-05 13:26:45 +0100

    navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
-console.log("bla");
+angular.module('app', ['ngRoute'])
+
+.config(function($routeProvider) {
+
+  $routeProvider
+    .when('/', {
+      controller:'HomeController',
+      templateUrl:'html/home.html',
+    })
+    .when('/service', {
+      controller:'ServiceController',
+      templateUrl:'html/service.html',
+    })
+    .when('/connection', {
+      controller:'ConnectionController',
+      templateUrl:'html/connection.html',
+    })
+    .when('/bridges', {
+      controller:'BridgesController',
+      templateUrl:'html/bridges.html',
+    })
+    .when('/topology', {
+      controller:'TopologyController',
+      templateUrl:'html/topology.html',
+    })
+    .when('/logs', {
+      controller:'LogsController',
+      templateUrl:'html/logs.html',
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+.controller('MainController', function($scope) {
+  $scope.menu=[
+    {href:"#/service" , label:"Service" , menu: [{href:"#/connection" , label:"Connection" }]},
+    {href:"#/connection" , label:"Connection" },
+    {href:"#/bridges" , label:"Bridges" },
+    {href:"#/topology" , label:"Topology" },
+    {href:"#/logs" , label:"Logs" }
+  ]
+
+  console.log("MainController");
+})
+.controller('HomeController', function($scope) {
+  console.log("HomeController");
+  $scope.introduction="blabla";
+})
+.controller('ServiceController', function() {
+  console.log("ServiceController");
+})
+.controller('ConnectionController', function() {
+  console.log("ConnectionController");
+
+})
+.controller('BridgesController', function() {
+  console.log("BridgesController");
+
+})
+.controller('TopologyController', function() {
+  console.log("TopologyController");
+
+})
+.controller('LogsController', function() {
+  console.log("LogsController");
+
+})
