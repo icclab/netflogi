ec390f43b92b3cb8180da88fcbb5fbdf912ff72c
Denis Gerold Baudinot <badi@zhaw.ch>
2016-02-19 16:17:34 +0100

    neutron & keystone calls

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ var _ = require("underscore"),
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post("/api/create-service-chain", function(req, res) {
-	console.log("/api/createServiceChain", req.body);
+var neutronPorts = [
+	{ id: "1c7811dc-e0b1-4d1e-9089-c203df328157", macAddress: "fa:16:3e:47:47:00", ips: [{"subnet_id": "50d00739-172c-4ca5-9c0d-79478b27bc9f", "ip_address": "12.12.12.2"}]},
+	{ id: "38aa2be8-dec7-49b3-94e6-805bb23c30fd", macAddress: "fa:16:3e:a0:25:f2", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.4"}]},
+	{ id: "3b842df8-e92a-4d53-b394-fc575d8d37e5", macAddress: "fa:16:3e:5d:02:57", ips: [{"subnet_id": "61ae89f8-5297-4d5a-96fa-c6c3e2a1fa74", "ip_address": "10.12.0.3"}]},
+	{ id: "3cac74fd-f24d-4b40-8344-1a86af85113e", macAddress: "fa:16:3e:bb:a4:ca", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.1"}]},
+	{ id: "78058861-9079-4b9e-925f-81f4a0ac3e27", macAddress: "fa:16:3e:62:b4:f2", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.7"}]},
+	{ id: "7fc1733c-f8d3-47cf-b755-0b08374a0e55", macAddress: "fa:16:3e:bb:1f:e0", ips: [{"subnet_id": "50d00739-172c-4ca5-9c0d-79478b27bc9f", "ip_address": "12.12.12.4"}]},
+	{ id: "8075c307-4df9-4421-b65b-b2d871f83f56", macAddress: "fa:16:3e:3d:36:b6", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.3"}]},
+	{ id: "87b30d9c-e895-42c0-8ea1-09c5087c2207", macAddress: "fa:16:3e:79:75:3f", ips: [{"subnet_id": "61ae89f8-5297-4d5a-96fa-c6c3e2a1fa74", "ip_address": "10.12.0.2"}]},
+	{ id: "96ccf08f-6aa1-4b88-b853-4dfd46853a22", macAddress: "fa:16:3e:e1:2c:58", ips: [{"subnet_id": "50d00739-172c-4ca5-9c0d-79478b27bc9f", "ip_address": "12.12.12.5"}]}
+];
+
+app.post("/api/service-chain", function(req, res) {
+	console.log("/api/service-chain", req.body);
 	var postRequest = http.request({
 		host: config.host,
 		port: config.port,
@@ -25,20 +37,95 @@ app.post("/api/create-service-chain", function(req, res) {
 		console.log("netfloc response");
 		netflocRes.on('data', function (chunk) {
 			console.log('Response: ' + chunk);
-			res.send(chunk);
+			res.status(200).send(chunk);
       	});
       	netflocRes.on("error", function(err) {
       		console.log("netfloc error:res", err);
+      		res.status(500).send(err);
       	});
 	})
 	postRequest.on("error", function(err) {
 		console.log("netfloc error:req", err);
+		res.status(400).send(err);
+	});
+	postRequest.write(JSON.stringify(req.body));
+	postRequest.end();
+});
+
+var keystone = {
+    "auth": {
+        "passwordCredentials": {
+            "password": "6d12c209c69946ce",
+            "username": "admin"
+        },
+        "tenantName": "admin"
+    }
+};
+
+var token;
+
+var getToken = function() {
+	var postRequest = http.request({
+		host: config.keystoneHost,
+		port: config.keystonePort,
+		//auth: config.auth,
+		method: "POST",
+		path: "/v2.0/ports",
+		headers: {
+			'Content-Type': 'application/json',
+			//'Content-Length': Buffer.byteLength(req.body)
+		}
+	}, function(keyStoneRes) {
+		console.log("keystone response");
+		keyStoneRes.on('data', function (chunk) {
+			console.log('Response: ' + chunk);
+			token = chunk;
+			console.log(token);
+      	});
+      	keyStoneRes.on("error", function(err) {
+      		console.log("keystone error:res", err);
+      	});
+	})
+	postRequest.on("error", function(err) {
+		console.log("keystone error:req", err);
+		res.send(err);
+	});
+	postRequest.write(JSON.stringify(keystone));
+	postRequest.end();
+};
+
+getToken();
+
+app.get("/api/neutron-ports", function(req, res) {
+	console.log("/api/neutron-ports", req.body);
+	var postRequest = http.request({
+		host: config.neutronHost,
+		port: config.neutronPort,
+		//auth: config.auth,
+		method: "POST",
+		path: "/v2.0/ports",
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Token ' + token
+      }
+	}, function(neutronRes) {
+		console.log("neutron response");
+		neutronRes.on('data', function (chunk) {
+			console.log('Response: ' + chunk);
+			res.send(chunk);
+      	});
+      	neutronRes.on("error", function(err) {
+      		console.log("neutron error:res", err);
+      	});
+	})
+	postRequest.on("error", function(err) {
+		console.log("neutron error:req", err);
 		res.send(err);
 	});
 	postRequest.write(JSON.stringify(req.body));
 	postRequest.end();
 });
 
-app.listen(80, function() {
-	console.log("listening on port 80");
+app.listen(3000, function() {
+	console.log("listening on port 3000");
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,187 +2,212 @@ angular.module('app', ['ui.bootstrap', 'ngRoute'])
 
 .config(function($routeProvider) {
 
-  $routeProvider
-    .when('/', {
-      controller:'HomeController',
-      templateUrl:'html/home.html',
-    })
-    .when('/service-function-chaining', {
-      controller:'ServiceFunctionChainingController',
-      templateUrl:'html/service-function-chaining.html',
-    })
-    .when('/connection', {
-      controller:'ConnectionController',
-      templateUrl:'html/connection.html',
-    })
-    .when('/bridges', {
-      controller:'BridgesController',
-      templateUrl:'html/bridges.html',
-    })
-    .when('/topology', {
-      controller:'TopologyController',
-      templateUrl:'html/topology.html',
-    })
-    .when('/logs', {
-      controller:'LogsController',
-      templateUrl:'html/logs.html',
-    })
-    .when('/bridgepattern', {
-      controller:'BridgePatternController',
-      templateUrl:'html/bridgepatterns.html',
-    })
-    .when('/chainpattern', {
-      controller:'ChainPatternController',
-      templateUrl:'html/chainpatterns.html',
-    })
-    .when('/subbridges', {
-      controller:'SubBridgesController',
-      templateUrl:'html/subbridges.html',
-    })
-    .when('/flowpath', {
-      controller:'FlowPathController',
-      templateUrl:'html/flowpath.html',
-    })
-    .when('/flowpatterns', {
-      controller:'FlowPatternController',
-      templateUrl:'html/flowpatterns.html',
-    })
-    .otherwise({
-      redirectTo:'/'
-    });
+	$routeProvider
+	.when('/', {
+		controller:'HomeController',
+		templateUrl:'html/home.html',
+	})
+	.when('/service-function-chaining', {
+		controller:'ServiceFunctionChainingController',
+		templateUrl:'html/service-function-chaining.html',
+	})
+	.when('/connection', {
+		controller:'ConnectionController',
+		templateUrl:'html/connection.html',
+	})
+	.when('/bridges', {
+		controller:'BridgesController',
+		templateUrl:'html/bridges.html',
+	})
+	.when('/topology', {
+		controller:'TopologyController',
+		templateUrl:'html/topology.html',
+	})
+	.when('/logs', {
+		controller:'LogsController',
+		templateUrl:'html/logs.html',
+	})
+	.when('/bridgepattern', {
+		controller:'BridgePatternController',
+		templateUrl:'html/bridgepatterns.html',
+	})
+	.when('/chainpattern', {
+		controller:'ChainPatternController',
+		templateUrl:'html/chainpatterns.html',
+	})
+	.when('/subbridges', {
+		controller:'SubBridgesController',
+		templateUrl:'html/subbridges.html',
+	})
+	.when('/flowpath', {
+		controller:'FlowPathController',
+		templateUrl:'html/flowpath.html',
+	})
+	.when('/flowpatterns', {
+		controller:'FlowPatternController',
+		templateUrl:'html/flowpatterns.html',
+	})
+	.otherwise({
+		redirectTo:'/'
+	});
 })
 .service('netfloc', function($http) {
-  var that = this;
-  var configurated = false;
-  var settings = {
-    url: {
-      createServiceChain: '/api/create-service-chain'
-    }
-  };
-  this.config = function(options) {
-    if (configurated) { return that; }
-    _.extend(settings, options); configurated = true;
-    return that;
-  };
-  this.createServiceChain = function(neutronPorts, fn) {
-    $http({
-      method: 'POST',
-      url: settings.url,
-      data: {
-        "input": {
-          "neutron-ports": neutronPorts
-        }
-      }
-    }).then(fn);
-    return that;
-  };
-  this.getNeutronPorts = function(fn) {
-    var data = [
-      { id: "1c7811dc-e0b1-4d1e-9089-c203df328157", macAddress: "fa:16:3e:47:47:00", ips: [{"subnet_id": "50d00739-172c-4ca5-9c0d-79478b27bc9f", "ip_address": "12.12.12.2"}]},
-      { id: "38aa2be8-dec7-49b3-94e6-805bb23c30fd", macAddress: "fa:16:3e:a0:25:f2", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.4"}]},
-      { id: "3b842df8-e92a-4d53-b394-fc575d8d37e5", macAddress: "fa:16:3e:5d:02:57", ips: [{"subnet_id": "61ae89f8-5297-4d5a-96fa-c6c3e2a1fa74", "ip_address": "10.12.0.3"}]},
-      { id: "3cac74fd-f24d-4b40-8344-1a86af85113e", macAddress: "fa:16:3e:bb:a4:ca", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.1"}]},
-      { id: "78058861-9079-4b9e-925f-81f4a0ac3e27", macAddress: "fa:16:3e:62:b4:f2", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.7"}]},
-      { id: "7fc1733c-f8d3-47cf-b755-0b08374a0e55", macAddress: "fa:16:3e:bb:1f:e0", ips: [{"subnet_id": "50d00739-172c-4ca5-9c0d-79478b27bc9f", "ip_address": "12.12.12.4"}]},
-      { id: "8075c307-4df9-4421-b65b-b2d871f83f56", macAddress: "fa:16:3e:3d:36:b6", ips: [{"subnet_id": "fcd1456f-15dd-4e0b-9821-8036858a117c", "ip_address": "10.10.10.3"}]},
-      { id: "87b30d9c-e895-42c0-8ea1-09c5087c2207", macAddress: "fa:16:3e:79:75:3f", ips: [{"subnet_id": "61ae89f8-5297-4d5a-96fa-c6c3e2a1fa74", "ip_address": "10.12.0.2"}]},
-      { id: "96ccf08f-6aa1-4b88-b853-4dfd46853a22", macAddress: "fa:16:3e:e1:2c:58", ips: [{"subnet_id": "50d00739-172c-4ca5-9c0d-79478b27bc9f", "ip_address": "12.12.12.5"}]}
-    ];
-    fn(data);
-  };
+	var that = this;
+	var configurated = false;
+	var settings = {
+		url: {
+			serviceChain: '/api/service-chain',
+			neutronPorts: '/api/neutron-ports'
+		}
+	};
+	this.config = function(options) {
+		if (configurated) { return that; }
+		_.extend(settings, options); configurated = true;
+		return that;
+	};
+	this.createServiceChain = function(neutronPorts) {
+		return $http({
+			method: 'POST',
+			url: settings.url.serviceChain,
+			data: {
+				"input": {
+					"neutron-ports": neutronPorts
+				}
+			}
+		});
+	};
+	this.getNeutronPorts = function() {
+		return $http({
+			method: "GET",
+			url: settings.url.neutronPorts
+		});
+	};
 })
 .controller('MainController', function($scope) {
-  $scope.menu=[
-    {href:"#/topology" , label:"Topology" },
-    {href:"#/logs" , label:"Logs" }
-  ];
-  $scope.menuService = {label:"Service" , menu: [{href:"#/service-function-chaining" , label:"Service Function Chaining"} , {href:"#/chainpattern" , label:"Chain Pattern" }]};
-  $scope.menuConnection = {label:"Connection" , menu: [{href:"#/flowpath" , label:"Flow Path" } , {href:"#/flowpatterns" , label:"Flow Pattern" }] };
-  $scope.menuBridges = {label:"Bridges" , menu: [{href:"#/subbridges" , label:"Bridges" } , {href:"#/bridgepattern" , label:"Bridge Pattern" }]};
-  $scope.menuTopology = {};
-  $scope.menuLogs = {};
-  console.log("MainController");
+	$scope.menu=[
+	{href:"#/topology" , label:"Topology" },
+	{href:"#/logs" , label:"Logs" }
+	];
+	$scope.menuService = {label:"Service" , menu: [{href:"#/service-function-chaining" , label:"Service Function Chaining"} , {href:"#/chainpattern" , label:"Chain Pattern" }]};
+	$scope.menuConnection = {label:"Connection" , menu: [{href:"#/flowpath" , label:"Flow Path" } , {href:"#/flowpatterns" , label:"Flow Pattern" }] };
+	$scope.menuBridges = {label:"Bridges" , menu: [{href:"#/subbridges" , label:"Bridges" } , {href:"#/bridgepattern" , label:"Bridge Pattern" }]};
+	$scope.menuTopology = {};
+	$scope.menuLogs = {};
+	console.log("MainController");
 })
-.controller('HomeCoselectedOrderntroller', function($scope) {
-  console.log("HomeController");
-  $scope.introduction="blabla";
+.controller('HomeController', function($scope) {
+	console.log("HomeController");
+	$scope.introduction="blabla";
 })
 .controller('ServiceFunctionChainingController', function($scope, netfloc) {
-  netfloc.getNeutronPorts(function(ports){
-    $scope.neutronPorts = _.map(ports, function(port) {
-      port.selectedOrder = 0;
-      port.dropDownToggled = function(open) {
-        console.log("toggled", this, open);
-      };
-      return port;
-    });
-  });
-  $scope.maxChainOrderNr = 0;
-  $scope.applyMaxChainOrder = function() {
-    $scope.maxChainOrder = Array.apply(null, {length: $scope.maxChainOrderNr+1}).map(Number.call, Number);
-    console.log("new maxChainOrder", $scope.maxChainOrder);
-  };
-  $scope.maxChainOrder = [null];
-  $scope.createServiceChain = function() {
-    var serviceChain = _.filter($scope.neutronPorts, function(port){
-       return port.selectedOrder != 0;
-         });
+	console.log("ServiceFunctionChainingController");
 
-    console.log("filtered:", serviceChain);
-    serviceChain = _.sortBy(serviceChain, 'selectedOrder');
-   console.log("sorted:", serviceChain);
+	$scope.fetchNeutronPorts = function() {
+		netfloc.getNeutronPorts().then(function(ports){
+			console.log("neutron-ports", ports.data);
+			$scope.neutronPorts = _.map(ports.data, function(port) {
+				port.selectedOrder = 0;
+				port.dropDownToggled = function(open) {
+					console.log("toggled", this, open);
+				};
+				return port;
+			});
+		});
+	};
+	$scope.fetchNeutronPorts();
 
-    serviceChain = _.map(serviceChain, function(neutronPort){
-      return neutronPort.id;
-    });
-    console.log("chained:", serviceChain);
-    var serviceChainString = serviceChain.join(",")
-    console.log("serviceChainString:", serviceChainString);
-    netfloc.createServiceChain(serviceChainString, function() {});
-  }//
-  console.log("ServiceFunctionChainingController");
+	var clearNeutronPorts = function() {
+		$scope.neutronPorts = _.map($scope.neutronPorts, function(port) {
+			console.log(port);
+			port.selectedOrder = 0;
+			return port;
+		});
+	};
 
-  $scope.selectOrder = function(port, order) {
-    port.selectedOrder = order;
-    $scope.maxChainOrder = _.filter($scope.maxChainOrder, function(chainOrder) {
-      return chainOrder != order;
-    });
-    console.log("select order for port", order, port);
-  }
+	$scope.getServiceChain = function(ports) {
+		return  _.map(_.sortBy(_.filter(ports, function(port){
+			return port.selectedOrder != 0;
+		}), 'selectedOrder'), function(port){
+			return port.id;
+		});
+	};
+
+	$scope.chainIsValid = function(ports, maxChainOrder) {
+		return $scope.getServiceChain(ports).length >= 4
+		&& maxChainOrder.length == 1
+		&& maxChainOrder[0] == 0;
+	};
+
+	$scope.maxChainOrderIsValid = function(nr) {
+		return nr >= 4 && nr % 2 == 0;
+	};
+
+	$scope.maxChainOrderNr = 0;
+	$scope.applyMaxChainOrder = function() {
+		clearNeutronPorts();
+		$scope.maxChainOrder = Array.apply(null, {length: $scope.maxChainOrderNr+1}).map(Number.call, Number);
+		console.log("apply maxChainOrder", $scope.maxChainOrder);
+	};
+
+	$scope.maxChainOrder = [null];
+	$scope.createServiceChain = function(ports) {
+		if (!$scope.chainIsValid(ports, $scope.maxChainOrder)) {
+			console.log("chain is not valid.");
+			return;
+		}
+		var serviceChainString = $scope.getServiceChain(ports).join(",");
+		console.log("serviceChainString:", serviceChainString);
+		netfloc.createServiceChain(serviceChainString)
+			.then(function(data) {
+				$scope.fetchNeutronPorts();
+				console.log(data);
+			})
+			.catch(function(err) {
+				console.error(err);
+			});
+	};
+
+	$scope.selectOrder = function(port, order) {
+		port.selectedOrder = order;
+		$scope.maxChainOrder = _.filter($scope.maxChainOrder, function(chainOrder) {
+			return chainOrder != order;
+		});
+		console.log("select order for port", order, port);
+	};
 })
 .controller('ChainPatternController', function() {
-  console.log("ChainPatternController");
+	console.log("ChainPatternController");
 
 })
 .controller('ConnectionController', function() {
-  console.log("ConnectionController");
+	console.log("ConnectionController");
 
 })
 .controller('FlowPathController', function() {
-  console.log("FlowPathController");
+	console.log("FlowPathController");
 
 })
 .controller('FlowPatternController', function() {
-  console.log("FlowPatternController");
+	console.log("FlowPatternController");
 
 })
 .controller('BridgesController', function() {
-  console.log("BridgesController");
+	console.log("BridgesController");
 
 })
 .controller('SubBridgesController', function() {
-  console.log("SubBridgesController");
+	console.log("SubBridgesController");
 
 })
 .controller('BridgePatternController', function() {
-  console.log("BridgePatternController");
+	console.log("BridgePatternController");
 
 })
 .controller('TopologyController', function() {
-  console.log("TopologyController");
+	console.log("TopologyController");
 
 })
 .controller('LogsController', function() {
-  console.log("LogsController");
+	console.log("LogsController");
 
 })
